21b9313ab196f675a68d694aa5ff2e8a527c9f16
Jenny Wu <jenwu05@gmail.com>
2014-04-23 17:40:50 -0700

    Added name

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,6 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>"}
 	validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
+	validates :description, presence: true
+
 end
